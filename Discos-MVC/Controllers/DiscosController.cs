596c2bea7b7c832cbc045bfc9f73990f470af21d
Nicolas Spalla <NicolasASpalla@gmail.com>
2025-10-05 18:46:00 +0200

    v1.0 repo de discos MVC

---
+++ Discos-MVC/Controllers/DiscosController.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+using dominio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using negocio;
+
+namespace Discos_MVC.Controllers
+{
+    public class DiscosController : Controller
+    {
+        // GET: DiscosController
+        public ActionResult Index()
+        {
+            DiscoNegocio negocio = new DiscoNegocio();
+            var lista = negocio.listar();
+            
+            return View(lista);
+        }
+
+        // GET: DiscosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DiscosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DiscosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DiscosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            EstiloNegocio estiloNegocio = new EstiloNegocio();
+            TipoEdicionNegocio tipoEdicionNegocio = new TipoEdicionNegocio();
+            DiscoNegocio discoNegocio = new DiscoNegocio();
+            var disco = discoNegocio.listar().Find(p => p.Id == id);
+            if (disco != null)
+            {
+                var listaEst = estiloNegocio.listar();
+                var listaEdi = tipoEdicionNegocio.listar();
+                ViewBag.estilos = new SelectList(listaEst, "Id", "Descripcion", disco.Estilo.Id);
+                ViewBag.edicion = new SelectList(listaEdi, "Id", "Descripcion", disco.TipoEdicion.Id);
+
+
+                return View(disco);
+            }
+            return View("Index");
+        }
+
+        // POST: DiscosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Disco disco)
+        {
+            try
+            {
+                DiscoNegocio negocio = new DiscoNegocio();
+                negocio.modificar(disco);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DiscosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DiscosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ dominio/Disco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Disco
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El titulo del disco es requerido")]
+        public string Titulo { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime FechaLanzamiento { get; set; }
+        public int CantidadCanciones { get; set; }
+        [DisplayName("Imagen"), Required(ErrorMessage ="Debe insertar una imagen para el disco")]
+        public string UrlTapa { get; set; }
+        public Estilo Estilo { get; set; }
+        [DisplayName("Edicion")]
+        public TipoEdicion TipoEdicion { get; set; }
+    }
+}
